fe3c0c797192b88b67432a4d2b671eeb50b69028
shery1-11 <slee92@risd.edu>
2025-05-02 12:17:51 -0400

    12:17

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ function togglePopup(el) {
     const popupText = el.querySelector('.popuptext');
     popupText.classList.toggle('show');
   }
+
+  let myWindow;
+  let bigWindow;
+
+      function openmyWin() {
+      myWindow = window.open("https://shery1-11.github.io/Project2/", "", "width=500, height=100");
+      }
+
+      function openbigWin(){
+      bigWindow = window.open("https://shery1-11.github.io/Project2/");
+      }
   
